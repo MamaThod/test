576bb5a725cec91e8f2ebd02964a70659c891978
MamaThod <taytheevil@gmail.com>
2023-10-31 14:42:48 +0700

    test

---
+++ index.js
@@ -0,0 +1,346 @@
+var product = [{
+    id: 1,
+    img: './imgs/product/1.png',
+    name: 'IPhone15',
+    price: 60000,
+    itm: 2500,
+    downpayment: 25000,
+    description: '!! ใช้บัตร ประชาชนใบเดียว !!',
+    type: 'apple'
+}, {
+    id: 2,
+    img: '/imgs/product/2.png',
+    name: 'ROG Phone 7 Ultimate',
+    price: 8000,
+    itm: 2500,
+    downpayment: 5000,
+    description: '!! ใช้บัตร ประชาชนใบเดียว !!',
+    type: 'rog'
+}, {
+    id: 3,
+    img: 'https://asia-exstatic-vivofs.vivo.com/PSee2l50xoirPK7y/1687940221913/c49570d5a75b43132b9ba732276c7ab0.png',
+    name: 'vivo Y27 4G',
+    price: 9000,
+    itm: 2500,
+    downpayment: 4000,
+    description: '!! ใช้บัตร ประชาชนใบเดียว !!',
+    type: 'vivo'
+}, {
+    id: 4,
+    img: 'https://static-jaymart.com/ecom/public/1yGC4eCcEGr1zbhi6UmUBYLfwex.jpg',
+    name: 'OPPO A16 4/64GB',
+    price: 10000,
+    itm: 2500,
+    downpayment: 5000,
+    description: '!! ใช้บัตร ประชาชนใบเดียว !!',
+    type: 'oppo'
+}, {
+    id: 5,
+    img: 'https://static-jaymart.com/ecom/public/1yGC4eCcEGr1zbhi6UmUBYLfwex.jpg',
+    name: 'OPPO A16 4/64GB',
+    price: 10000,
+    itm: 2500,
+    downpayment: 5000,
+    description: '!! ใช้บัตร ประชาชนใบเดียว !!',
+    type: 'oppo'
+}, {
+    id: 6,
+    img: 'https://static-jaymart.com/ecom/public/1yGC4eCcEGr1zbhi6UmUBYLfwex.jpg',
+    name: 'OPPO A16 4/64GB',
+    price: 10000,
+    itm: 2500,
+    downpayment: 5000,
+    description: '!! ใช้บัตร ประชาชนใบเดียว !!',
+    type: 'oppo'
+}, {
+    id: 7,
+    img: './imgs/product/1.png',
+    name: 'IPhone15',
+    price: 60000,
+    itm: 2500,
+    downpayment: 25000,
+    description: '!! ใช้บัตร ประชาชนใบเดียว !!',
+    type: 'apple'
+}, {
+    id: 8,
+    img: './imgs/product/1.png',
+    name: 'IPhone15',
+    price: 60000,
+    itm: 2500,
+    downpayment: 25000,
+    description: '!! ใช้บัตร ประชาชนใบเดียว !!',
+    type: 'apple'
+}, {
+    id: 9,
+    img: './imgs/product/1.png',
+    name: 'IPhone15',
+    price: 60000,
+    itm: 2500,
+    downpayment: 25000,
+    description: '!! ใช้บัตร ประชาชนใบเดียว !!',
+    type: 'apple'
+}, {
+    id: 10,
+    img: './imgs/product/1.png',
+    name: 'IPhone15',
+    price: 60000,
+    itm: 2500,
+    downpayment: 25000,
+    description: '!! ใช้บัตร ประชาชนใบเดียว !!',
+    type: 'apple'
+}];
+
+var product2 = [{
+    id: 1,
+    img: './imgs/product/ipad/1.png',
+    name: 'iPad gen10',
+    price: 18000,
+    itm: 1500,
+    downpayment: 8000,
+    description: '!! ใช้บัตร ประชาชนใบเดียว !!',
+    type: 'apple'
+},{
+    id: 2,
+    img: './imgs/product/ipad/2.png',
+    name: 'iPad gen9',
+    price: 15000,
+    itm: 1000,
+    downpayment: 6000,
+    description: '!! ใช้บัตร ประชาชนใบเดียว !!',
+    type: 'apple'
+},{
+    id: 3,
+    img: './imgs/product/ipad/1.png',
+    name: 'iPad gen10',
+    price: 18000,
+    itm: 1500,
+    downpayment: 8000,
+    description: '!! ใช้บัตร ประชาชนใบเดียว !!',
+    type: 'apple'
+},{
+    id: 4,
+    img: './imgs/product/ipad/1.png',
+    name: 'iPad gen10 asdasdasdasdsadsadsad',
+    price: 18000,
+    itm: 1500,
+    downpayment: 8000,
+    description: '!! ใช้บัตร ประชาชนใบเดียว !!',
+    type: 'apple'
+}];
+
+var product3 = [{
+    id: 1,
+    img: './imgs/product/powerbank/1.png',
+    name: 'REMAX infinity  15000 mAh',
+    price: 1200,
+    type: 'apple'
+},{
+    id: 2,
+    img: './imgs/product/powerbank/2.png',
+    name: 'AUKEY POWERBANK POWERPLUS SPRINT BLACK',
+    price: 1200,
+    type: 'apple'
+},{
+    id: 3,
+    img: './imgs/product/powerbank/1.png',
+    name: 'REMAX infinity QC3.0+PD18W(MAX) Power Bank 15000 mAh',
+    price: 1390,
+    type: 'apple'
+},{
+    id: 4,
+    img: './imgs/product/powerbank/1.png',
+    name: 'REMAX infinity QC3.0+PD18W(MAX) Power Bank 15000 mAh',
+    price: 1200,
+    type: 'apple'
+}];
+
+var product4 = [{
+    id: 1,
+    img: './imgs/product/case/1.png',
+    name: 'iPhone Case - One Piece SOLO',
+    price: 690,
+    type: 'apple'
+},{
+    id: 2,
+    img: './imgs/product/case/2.png',
+    name: 'iPhone Case - One Piece LUFI',
+    price: 690,
+    type: 'apple'
+},{
+    id: 3,
+    img: './imgs/product/case/1.png',
+    name: 'iPhone Case - One Piece SOLO',
+    price: 690,
+    type: 'apple'
+},{
+    id: 4,
+    img: './imgs/product/case/2.png',
+    name: 'iPhone Case - One Piece LUFI',
+    price: 690,
+    type: 'apple'
+}];
+
+
+
+$(document).ready(() => {
+    var html = '';
+    for (let k = 0; k < product.length; k++) {
+        html +=`<div onclick="openproductmb(${k})" class="product-items ${product[k].type}">
+                    <img class="product-img" src="${product[k].img}" alt="">
+                    <div class="protext">
+                    <p class="name">${product[k].name}</p>
+                    <p class="pddd">${numberWithCommas(product[k].price)} THB</p>
+                    <p class="itm" >${numberWithCommas(product[k].itm)}/ เดือน</p>
+                    </div>
+                </div>`
+    }
+    $('#productlist').html(html);
+})
+
+
+$(document).ready(() => {
+    var html = '';
+    for (let j = 0; j < product2.length; j++) {
+        html +=`<div onclick="openproductipad(${j})" class="product-items ${product2[j].type}">
+                    <img class="product-img" src="${product2[j].img}" alt="">
+                    <div class="protext">
+                    <p class="name">${product2[j].name}</p>
+                    <p class="pddd">${numberWithCommas(product2[j].price)} THB</p>
+                    <p class="itm" >${numberWithCommas(product2[j].itm)}/ เดือน</p>
+                    </div>
+                </div>`
+    }
+    $('#productipad').html(html);
+})
+
+$(document).ready(() => {
+    var html = '';
+    for (let m = 0; m < product3.length; m++) {
+        html +=`<div onclick="openpropowerbank(${m})" class="product-items ${product3[m].type}">
+                    <img class="product-img" src="${product3[m].img}" alt="">
+                    <div class="protext">
+                    <p class="name">${product3[m].name}</p>
+                    <p class="pddd">${numberWithCommas(product3[m].price)} THB</p>
+                    </div>
+                </div>`
+    }
+    $('#productpowerbank').html(html);
+})
+
+$(document).ready(() => {
+    var html = '';
+    for (let c = 0; c < product4.length; c++) {
+        html +=`<div onclick="productcase(${c})" class="product-items ${product4[c].type}">
+                    <img class="product-img" src="${product4[c].img}" alt="">
+                    <div class="protext">
+                    <p class="name">${product4[c].name}</p>
+                    <p class="pddd">${numberWithCommas(product4[c].price)} THB</p>
+                    </div>
+                </div>`
+    }
+    $('#productcase').html(html);
+})
+
+
+
+function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
+function searchob(elem) {
+    var value = $('#'+elem.id).val()
+    console.log(value)
+    
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+        if(product[i].name.includes(value) ) {
+            html +=`<div onclick="openproduct(${i})" class="product-items ${product[i].type}">
+                    <img class="product-img" src="${product[i].img}" alt="">
+                    <p style="font-size: 1.2vw;">${product[i].name}</p>
+                    <p style="font-size: 1vw;">${numberWithCommas(product[i].price)} THB</p>
+                </div>`
+        }
+    }
+    if(html == '') {
+        $("#productlist").html(`<p>Not found product</p>`);
+    } else {
+        $('#productlist').html(html);
+    }
+    
+
+}
+
+function searchproduct(param) {
+    console.log(param)
+    $(".product-items").css('display', 'none')
+    if(param == 'all') {
+        $(".product-items").css('display', 'block')
+    } else {
+        $("."+param).css('display','block')
+    }
+}
+
+var productindex = 0;
+function openproductmb(index) {
+    productindex = index;
+    console.log(productindex)
+    $("#model").css('display', 'flex')
+    $("#mdd-img").attr('src', product[index].img);
+    $("#mdd-name").text(product[index].name);
+    $("#mdd-price").text( numberWithCommas(product[index].price) + ' THB');
+    $("#mdd-down").text( 'เงินดาวน์ ' + numberWithCommas(product[index].downpayment));
+    $("#mdd-itm").text( numberWithCommas(product[index].itm) + ' /เดือน');
+    
+}
+
+function openproductipad(index) {
+    productindex = index;
+    console.log(productindex)
+    $("#model").css('display', 'flex')
+    $("#mdd-img").attr('src', product2[index].img);
+    $("#mdd-name").text(product2[index].name);
+    $("#mdd-price").text( numberWithCommas(product2[index].price) + ' THB');
+    $("#mdd-down").text( 'เงินดาวน์ ' + numberWithCommas(product2[index].downpayment));
+    $("#mdd-itm").text( numberWithCommas(product2[index].itm) + ' /เดือน');
+    
+}
+
+function openpropowerbank(index) {
+    productindex = index;
+    console.log(productindex)
+    $("#modelpower").css('display', 'flex')
+    $("#mdd-img2").attr('src', product3[index].img);
+    $("#mdd-name2").text(product3[index].name);
+    $("#mdd-price2").text( numberWithCommas(product3[index].price) + ' THB');  
+}
+
+function productcase(index) {
+    productindex = index;
+    console.log(productindex)
+    $("#modelpower").css('display', 'flex')
+    $("#mdd-img2").attr('src', product4[index].img);
+    $("#mdd-name2").text(product4[index].name);
+    $("#mdd-price2").text( numberWithCommas(product4[index].price) + ' THB');  
+}
+
+function closemodal() {
+    $(".model").css('display','none')
+}
+
+function closemenumb() {
+    $(".menu-mb").css('right','-120%')
+    $(".menu-mb").css('background-color','transparent')
+}
+
+function openmenumb() {
+    $(".menu-mb").css('right','0px')
+    $(".menu-mb").css('background-color','white')
+    
+}
+
+
+
